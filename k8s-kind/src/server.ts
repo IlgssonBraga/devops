324883eb8c0df3e83ef47e7db6b82ef87e1f45ef
Ilgsson Braga <ilgssonbraga@gmail.com>
2021-08-29 15:30:58 -0300

    feat: inject file configmap

--- k8s-kind/src/server.ts
+++ k8s-kind/src/server.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { readFileSync } from "fs";
+import { join } from "path";
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => res.send(`<h1>Hello, ${process.env.NAME}!!!</h1>`));
+app.get("/", (req, res) => {
+  //   const greetings = `<h1>Hello, ${process.env.NAME}!!!</h1>`;
+  const data = readFileSync(join(__dirname, "../myfamily/myfamily.txt"), {
+    encoding: "utf8",
+    flag: "r",
+  });
+  return res.send(`<h1>Hello, ${process.env.NAME}!!!</h1> 
+  <h3>Membros da família</h3>
+  ${data}`);
+});
 
 app.listen(3333);
  
